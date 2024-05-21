ecc797264839f6f7693071ffd2b8748788e89218
mabel <bigbelk@live.ru>
2024-05-21 08:12:28 +0300

    Updates

---
+++ _app/immutable/entry/start.b45sR5B9.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CFU86Jm6.js";export{t as start};
--- _app/immutable/nodes/4.DHGlY0la.js
+++ _app/immutable/nodes/4.DHGlY0la.js
@@ -1,16 +1,16 @@
 import{s,n as r}from"../chunks/scheduler.CtbWrGNo.js";import{S as i,i as c,n as l,o as m,p,l as u,m as h,q as b}from"../chunks/index.D_d3RPrM.js";import{A as g}from"../chunks/index.CHItn9hs.js";const f=!0,y=!1,$=Object.freeze(Object.defineProperty({__proto__:null,csr:y,ssr:f},Symbol.toStringTag,{value:"Module"})),_=`Здесь размещены длинные тексты (лонгриды) для Телеграм-канала [«Эхо русских революций»](https://t.me/channel_1917).
 
 # Содержание
-* [Материализм и эмпириогопницизм](/echo-1917/materializm)
-* [Квантовая механика для диссидентов](/echo-1917/political-honest-smart)
-* [Метаморфозы поэта Смертяшкина](/echo-1917/smertyashkin)
-* [Правда? Дайте две!](/echo-1917/pravda-i-shumhin)
-* [От Батума до батута](/echo-1917/batum)
-* [Ко дню рождения Карла Маркса](/echo-1917/dr-marksa)
-* [Темная лошадка Викжель](/echo-1917/vikjel)
-* [Остап Бендер на Гражданской войне](/echo-1917/ostap-na-grajdanskoy)
-* [Гарнитур мастера Мастера](/echo-1917/bulgakov-12-stulyev)
-* [Богемный пацифизм](/echo-1917/bogemniy-pacifizm)
+* [Материализм и эмпириогопницизм](./echo-1917/materializm)
+* [Квантовая механика для диссидентов](./echo-1917/political-honest-smart)
+* [Метаморфозы поэта Смертяшкина](./echo-1917/smertyashkin)
+* [Правда? Дайте две!](./echo-1917/pravda-i-shumhin)
+* [От Батума до батута](./echo-1917/batum)
+* [Ко дню рождения Карла Маркса](./echo-1917/dr-marksa)
+* [Темная лошадка Викжель](./echo-1917/vikjel)
+* [Остап Бендер на Гражданской войне](./echo-1917/ostap-na-grajdanskoy)
+* [Гарнитур мастера Мастера](/echo-1917./bulgakov-12-stulyev)
+* [Богемный пацифизм](./echo-1917/bogemniy-pacifizm)
 
 <!--
 Их можно также найти в моем [boosty-блоге](https://boosty.to/channel-1917), где есть все необходимое для того, чтобы финансово поддержать усилия автора по исследованию феноменов 1905-1907 и 1917 гг.
