82322c9814ba15a820d235778f2620cd699699b7
mabel <bigbelk@live.ru>
2024-05-21 10:19:05 +0300

    Updates

---
+++ _app/immutable/entry/start.CXWXWBbP.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.BSbDdwfg.js";export{t as start};
--- _app/immutable/nodes/4.B4itLHGs.js
+++ _app/immutable/nodes/4.B4itLHGs.js
@@ -1,16 +1,16 @@
-import{s,n as r}from"../chunks/scheduler.CtbWrGNo.js";import{S as i,i as c,n as l,o as m,p,l as u,m as h,q as b}from"../chunks/index.D_d3RPrM.js";import{A as g}from"../chunks/index.CHItn9hs.js";const f=!0,y=!1,$=Object.freeze(Object.defineProperty({__proto__:null,csr:y,ssr:f},Symbol.toStringTag,{value:"Module"})),_=`Здесь размещены длинные тексты (лонгриды) для Телеграм-канала [«Эхо русских революций»](https://t.me/channel_1917).
+import{s,n as r}from"../chunks/scheduler.CtbWrGNo.js";import{S as i,i as l,n as c,o as m,p,l as u,m as b,q as g}from"../chunks/index.D_d3RPrM.js";import{A as f}from"../chunks/index.CHItn9hs.js";const h=!0,y=!1,$=Object.freeze(Object.defineProperty({__proto__:null,csr:y,ssr:h},Symbol.toStringTag,{value:"Module"})),_=`Здесь размещены длинные тексты (лонгриды) для Телеграм-канала [«Эхо русских революций»](https://t.me/channel_1917).
 
 # Содержание
-* [Материализм и эмпириогопницизм](./echo-1917/materializm)
-* [Квантовая механика для диссидентов](./echo-1917/political-honest-smart)
-* [Метаморфозы поэта Смертяшкина](./echo-1917/smertyashkin)
-* [Правда? Дайте две!](./echo-1917/pravda-i-shumhin)
-* [От Батума до батута](./echo-1917/batum)
-* [Ко дню рождения Карла Маркса](./echo-1917/dr-marksa)
-* [Темная лошадка Викжель](./echo-1917/vikjel)
-* [Остап Бендер на Гражданской войне](./echo-1917/ostap-na-grajdanskoy)
-* [Гарнитур мастера Мастера](/echo-1917./bulgakov-12-stulyev)
-* [Богемный пацифизм](./echo-1917/bogemniy-pacifizm)
+* [Материализм и эмпириогопницизм](./materializm/)
+* [Квантовая механика для диссидентов](./political-honest-smart/)
+* [Метаморфозы поэта Смертяшкина](./smertyashkin/)
+* [Правда? Дайте две!](./pravda-i-shumhin/)
+* [От Батума до батута](./batum/)
+* [Ко дню рождения Карла Маркса](./dr-marksa/)
+* [Темная лошадка Викжель](./vikjel/)
+* [Остап Бендер на Гражданской войне](./ostap-na-grajdanskoy/)
+* [Гарнитур мастера Мастера](./bulgakov-12-stulyev/)
+* [Богемный пацифизм](./bogemniy-pacifizm/)
 
 <!--
 Их можно также найти в моем [boosty-блоге](https://boosty.to/channel-1917), где есть все необходимое для того, чтобы финансово поддержать усилия автора по исследованию феноменов 1905-1907 и 1917 гг.
@@ -31,4 +31,4 @@ import{s,n as r}from"../chunks/scheduler.CtbWrGNo.js";import{S as i,i as c,n as 
 * [Шелдон и Ильич](articles/sheldon)
 * [Бронепоезда Белой армии](articles/bronepoezd)
 -->
-`;function d(o){let e,t;return e=new g({props:{raw:_}}),{c(){l(e.$$.fragment)},l(n){m(e.$$.fragment,n)},m(n,a){p(e,n,a),t=!0},p:r,i(n){t||(u(e.$$.fragment,n),t=!0)},o(n){h(e.$$.fragment,n),t=!1},d(n){b(e,n)}}}class j extends i{constructor(e){super(),c(this,e,null,d,s,{})}}export{j as component,$ as universal};
+`;function d(a){let n,t;return n=new f({props:{raw:_}}),{c(){c(n.$$.fragment)},l(e){m(n.$$.fragment,e)},m(e,o){p(n,e,o),t=!0},p:r,i(e){t||(u(n.$$.fragment,e),t=!0)},o(e){b(n.$$.fragment,e),t=!1},d(e){g(n,e)}}}class j extends i{constructor(n){super(),l(this,n,null,d,s,{})}}export{j as component,$ as universal};
