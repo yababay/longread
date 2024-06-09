455b6e4bd9b699841229bfa3d39141209b1944d3
mabel <bigbelk@live.ru>
2024-06-09 23:23:10 +0300

    Updates

---
+++ _app/immutable/entry/start.DESXJmyM.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Bd4bUZ0o.js";export{t as start};
---
+++ _app/immutable/nodes/4.CRdusfes.js
@@ -0,0 +1,41 @@
+import{s,n as r}from"../chunks/scheduler.CtbWrGNo.js";import{S as i,i as l,n as c,o as m,p,l as u,m as d,q as h}from"../chunks/index.D_d3RPrM.js";import{A as b}from"../chunks/index.CHItn9hs.js";const g=!0,f=!1,$=Object.freeze(Object.defineProperty({__proto__:null,csr:f,ssr:g},Symbol.toStringTag,{value:"Module"})),y=`Здесь размещены длинные тексты (лонгриды) для Телеграм-канала [«Эхо русских революций»](https://t.me/channel_1917).
+
+# Содержание
+
+* [Макар Дедушкин и дурни](./dedushkin-i-durachok)
+* [Макар Дедушкин и письменный стол](./dedushkin-i-stol)
+* [Материализм и эмпириогопницизм](./materializm)
+* [Квантовая механика для диссидентов](./political-honest-smart)
+* [Метаморфозы поэта Смертяшкина](./smertyashkin)
+* [Правда? Дайте две!](./pravda-i-shumhin)
+* [От Батума до батута](./batum)
+* [Ко дню рождения Карла Маркса](./dr-marksa)
+* [Темная лошадка Викжель](./vikjel)
+* [Остап Бендер на Гражданской войне](./ostap-na-grajdanskoy)
+* [Гарнитур мастера Мастера](./bulgakov-12-stulyev)
+* [Богемный пацифизм](./bogemniy-pacifizm)
+
+<!--
+
+
+
+
+Их можно также найти в моем [boosty-блоге](https://boosty.to/channel-1917), где есть все необходимое для того, чтобы финансово поддержать усилия автора по исследованию феноменов 1905-1907 и 1917 гг.
+
+Этот \`README\`-файл является главной страницей сайта, размещенного на gh-pages, поэтому cсылки в разделе «Содержание» не работают в репозитории Github. Начните просмотр [здесь](https://yababay.github.io/boosty-1917).
+
+# Содержание
+
+* [Темная лошадка Викжель](./vikjel)
+* [Непредсказуемое прошлое Владимира Владимировича](./nepredskazuemoe-proshloe-vv)
+* [«Чтобы пузом на врага лечь...»](articles/puzom-na-vraga)
+* [Богемный милитаризм](articles/bogemniy-militarizm)
+* [Поэтиное сердце](articles/poetinoe-serdtse)
+* [Фрагменты из книги Марины Цветаевой «Вольный проезд»](articles/volniy-proezd)
+* [Как Марина Цветаева «убила свою собственную трехлетнюю дочь»](articles/ubila-rebenka)
+* [Бертран Рассел о Советах](articles/rassel-sovety)
+* [Почему я отключил комментарии](articles/no-comments)
+* [Шелдон и Ильич](articles/sheldon)
+* [Бронепоезда Белой армии](articles/bronepoezd)
+-->
+`;function _(a){let n,t;return n=new b({props:{raw:y}}),{c(){c(n.$$.fragment)},l(e){m(n.$$.fragment,e)},m(e,o){p(n,e,o),t=!0},p:r,i(e){t||(u(n.$$.fragment,e),t=!0)},o(e){d(n.$$.fragment,e),t=!1},d(e){h(n,e)}}}class j extends i{constructor(n){super(),l(this,n,null,_,s,{})}}export{j as component,$ as universal};
