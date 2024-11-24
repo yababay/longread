65f94ef78da9f443260ded495372553bc3628902
Михаил Беляков <bigbelk@live.ru>
2024-11-24 05:09:55 +0000

    Updates

--- _app/immutable/entry/start.CoXUga7b.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.CjqczcT6.js";export{t as start};
--- _app/immutable/nodes/4.BrfjAcdo.js
+++ _app/immutable/nodes/4.BrfjAcdo.js
@@ -1,7 +1,8 @@
-import{s as i,n as l}from"../chunks/scheduler.CtbWrGNo.js";import{S as c,i as m,n as p,s as u,o as d,f as h,r as f,d as o,p as _,g as b,l as g,m as y,q as v}from"../chunks/index.BP2C5PhF.js";import{A as k}from"../chunks/index.DfX1Nfvc.js";import{t as z}from"../chunks/index.CDQ24tm-.js";const $=!0,j=!1,M=Object.freeze(Object.defineProperty({__proto__:null,csr:j,ssr:$},Symbol.toStringTag,{value:"Module"})),x=`Здесь размещены длинные тексты (лонгриды) для Телеграм-канала [«Эхо русских революций»](https://t.me/channel_1917).
+import{s as i,n as l}from"../chunks/scheduler.CtbWrGNo.js";import{S as m,i as c,n as p,s as u,o as d,f as h,r as f,d as s,p as _,g as b,l as g,m as y,q as k}from"../chunks/index.BP2C5PhF.js";import{A as v}from"../chunks/index.DfX1Nfvc.js";import{t as z}from"../chunks/index.CDQ24tm-.js";const $=!0,j=!1,M=Object.freeze(Object.defineProperty({__proto__:null,csr:j,ssr:$},Symbol.toStringTag,{value:"Module"})),x=`Здесь размещены длинные тексты (лонгриды) для Телеграм-канала [«Эхо русских революций»](https://t.me/channel_1917).
 
 # Содержание
 
+* [Критика гадской программы](./manifest)
 * [Взрослые дрессированные люди](./stalin-prix)
 * [Город-сад: буржуйский и советский](./gorod-sad)
 * [Потомок Мак-Коя и нелюбовь к жизни](./mak-koy)
@@ -33,14 +34,12 @@ import{s as i,n as l}from"../chunks/scheduler.CtbWrGNo.js";import{S as c,i as m,
 
 * [Темная лошадка Викжель](./vikjel)
 * [Непредсказуемое прошлое Владимира Владимировича](./nepredskazuemoe-proshloe-vv)
-* [«Чтобы пузом на врага лечь...»](articles/puzom-na-vraga)
 * [Богемный милитаризм](articles/bogemniy-militarizm)
 * [Поэтиное сердце](articles/poetinoe-serdtse)
 * [Фрагменты из книги Марины Цветаевой «Вольный проезд»](articles/volniy-proezd)
 * [Как Марина Цветаева «убила свою собственную трехлетнюю дочь»](articles/ubila-rebenka)
-* [Бертран Рассел о Советах](articles/rassel-sovety)
 * [Почему я отключил комментарии](articles/no-comments)
 * [Шелдон и Ильич](articles/sheldon)
 * [Бронепоезда Белой армии](articles/bronepoezd)
 -->
-`;function S(r){let e,t,a;return e=new k({props:{raw:x}}),document.title=z,{c(){p(e.$$.fragment),t=u()},l(n){d(e.$$.fragment,n),t=h(n),f("svelte-1uo06u1",document.head).forEach(o)},m(n,s){_(e,n,s),b(n,t,s),a=!0},p:l,i(n){a||(g(e.$$.fragment,n),a=!0)},o(n){y(e.$$.fragment,n),a=!1},d(n){n&&o(t),v(e,n)}}}class O extends c{constructor(e){super(),m(this,e,null,S,i,{})}}export{O as component,M as universal};
+`;function S(r){let e,t,a;return e=new v({props:{raw:x}}),document.title=z,{c(){p(e.$$.fragment),t=u()},l(n){d(e.$$.fragment,n),t=h(n),f("svelte-1uo06u1",document.head).forEach(s)},m(n,o){_(e,n,o),b(n,t,o),a=!0},p:l,i(n){a||(g(e.$$.fragment,n),a=!0)},o(n){y(e.$$.fragment,n),a=!1},d(n){n&&s(t),k(e,n)}}}class O extends m{constructor(e){super(),c(this,e,null,S,i,{})}}export{O as component,M as universal};
