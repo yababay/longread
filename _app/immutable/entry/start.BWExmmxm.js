7982815d16b7bd7f3ad00a8e4d2d2d08d159fae6
Михаил Беляков <bigbelk@live.ru>
2025-02-19 05:57:13 +0300

    Updates

---
+++ _app/immutable/entry/start.BWExmmxm.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DLXME0rw.js";export{t as start};
--- _app/immutable/nodes/4.CFrZLexb.js
+++ _app/immutable/nodes/4.CFrZLexb.js
@@ -2,7 +2,7 @@ import{s as i,n as l}from"../chunks/scheduler.CtbWrGNo.js";import{S as m,i as c,
 
 # Содержание
 
-* [Как Александр Николаевич Бенуа стал гардеробщицей](./reincarnation)
+* [Как Александр Николаевич Бенуа стал гардеробщицей](./benua)
 * [СССР под микроскопом](./micro-ussr)
 * [Ленин в сентябре](./lenin-v-sentyabre)
 * [Великие дни](./velikie-dni)
