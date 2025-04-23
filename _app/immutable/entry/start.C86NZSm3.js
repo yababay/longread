e2c1245f1b7e1d1fa163dd7640ddcc4e64bdc249
Михаил Беляков <bigbelk@live.ru>
2025-04-23 05:05:03 +0000

    Updates

--- _app/immutable/entry/start.C86NZSm3.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/DUp-xbo6.js";export{o as load_css,r as start};
--- _app/immutable/nodes/4.D2fphDU-.js
+++ _app/immutable/nodes/4.D2fphDU-.js
@@ -1,10 +1,11 @@
-import{s as i,n as l}from"../chunks/DYjCt7Qj.js";import{S as c,i as m,d as s,n as d,l as u,m as p,o as h,a as v,p as k,g as b,r as y,q as f,j as g}from"../chunks/BK98yE9u.js";import{A as _}from"../chunks/CrHRKOku.js";import{t as z}from"../chunks/CDQ24tm-.js";const $=!0,j=!1,P=Object.freeze(Object.defineProperty({__proto__:null,csr:j,ssr:$},Symbol.toStringTag,{value:"Module"})),x=`Здесь размещены длинные тексты (лонгриды) для Телеграм-канала [«Эхо русских революций»](https://t.me/channel_1917).
+import{s as i,n as l}from"../chunks/DYjCt7Qj.js";import{S as c,i as m,d as s,n as d,l as u,m as p,o as h,a as v,p as k,g as b,r as g,q as y,j as f}from"../chunks/BK98yE9u.js";import{A as _}from"../chunks/CrHRKOku.js";import{t as z}from"../chunks/CDQ24tm-.js";const $=!0,j=!1,P=Object.freeze(Object.defineProperty({__proto__:null,csr:j,ssr:$},Symbol.toStringTag,{value:"Module"})),x=`Здесь размещены длинные тексты (лонгриды) для Телеграм-канала [«Эхо русских революций»](https://t.me/channel_1917).
 
 # Содержание
 
 <!-- * [Бизнес-план для большевиков](./business-plan)
 * [Индус, три Али](./indus) -->
 
+* [С пламенным большевистским игнором](./ignor)
 * [Сталин и сплавы](./stalin-i-splavy)
 * [Партийная арифметика](./arithmetic)
 * [Квакерско-большевистское братство](./bratstvo)
@@ -69,4 +70,4 @@ import{s as i,n as l}from"../chunks/DYjCt7Qj.js";import{S as c,i as m,d as s,n a
 * [Бронепоезда Белой армии](articles/bronepoezd)
 
 -->
-`;function S(r){let e,a,t;return e=new _({props:{raw:x}}),document.title=z,{c(){f(e.$$.fragment),a=g()},l(n){k(e.$$.fragment,n),a=b(n),y("svelte-1uo06u1",document.head).forEach(s)},m(n,o){h(e,n,o),v(n,a,o),t=!0},p:l,i(n){t||(p(e.$$.fragment,n),t=!0)},o(n){u(e.$$.fragment,n),t=!1},d(n){n&&s(a),d(e,n)}}}class C extends c{constructor(e){super(),m(this,e,null,S,i,{})}}export{C as component,P as universal};
+`;function S(r){let e,a,t;return e=new _({props:{raw:x}}),document.title=z,{c(){y(e.$$.fragment),a=f()},l(n){k(e.$$.fragment,n),a=b(n),g("svelte-1uo06u1",document.head).forEach(s)},m(n,o){h(e,n,o),v(n,a,o),t=!0},p:l,i(n){t||(p(e.$$.fragment,n),t=!0)},o(n){u(e.$$.fragment,n),t=!1},d(n){n&&s(a),d(e,n)}}}class C extends c{constructor(e){super(),m(this,e,null,S,i,{})}}export{C as component,P as universal};
