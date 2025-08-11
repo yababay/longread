d33cc18be395314d92a9a637559e73ed83243e02
Михаил Беляков <bigbelk@live.ru>
2025-08-11 12:45:35 +0000

    Updates

--- _app/immutable/chunks/CtuhAOuw.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./H6pGVG5W.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
